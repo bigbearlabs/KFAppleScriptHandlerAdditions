46158fc9c12d9111da4b9fc40ae626461a2b2ce6
Andy Park <andy@bigbearlabs.com>
2016-07-13 14:21:15 +0200

    add remaining resource to git.

---
+++ KFAppleScriptHandlerAdditions/test.js
@@ -0,0 +1,3 @@
+function doit(p) {
+	return p
+}
---
+++ KFAppleScriptHandlerAdditionsFramework/KFAppleScriptHandlerAdditions.h
@@ -0,0 +1,19 @@
+//
+//  KFAppleScriptHandlerAdditions.h
+//  KFAppleScriptHandlerAdditions
+//
+//  Created by ilo on 13/07/2016.
+//  Copyright © 2016 Big Bear Labs. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for KFAppleScriptHandlerAdditions.
+FOUNDATION_EXPORT double KFAppleScriptHandlerAdditionsVersionNumber;
+
+//! Project version string for KFAppleScriptHandlerAdditions.
+FOUNDATION_EXPORT const unsigned char KFAppleScriptHandlerAdditionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KFAppleScriptHandlerAdditions/PublicHeader.h>
+
+#import <KFAppleScriptHandlerAdditions/KFAppleScriptHandlerAdditionsCore.h>
